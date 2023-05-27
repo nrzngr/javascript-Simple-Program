def1053e803668c941e22616ec52542806d1022a
nrzngr <109433343+nrzngr@users.noreply.github.com>
2023-05-27 20:57:59 +0700

    Update letterFinder.js

--- letterFinder.js
+++ letterFinder.js
@@ -1,9 +1,6 @@
-function letterFinder(word, match){
-  for(var i = 0;i<=word.length;i++) {
-    if(word[i] == match) {
-      console.log(match, " found at ", i)
-    } else {
-      console.log(match, " not found in ", word)
+function letterFinder(word, match) {
+    for(var i=0;i<=word.length;i++){
+        word[i] == match ? console.log(match, " found at ", i) : console.log(match, " not found at ", i)
     }
-  }
 }
+letterFinder("test", "t")
